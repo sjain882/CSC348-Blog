7f37d4e256d32523e6722bd579ece645bc086708
sjain882 <43217178+sjain882@users.noreply.github.com>
2023-10-18 11:18:20 +0100

    Add example routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/blog', function () {
+    return "This is the blog page.";
+});
+
+Route::redirect('/blog2', '/blog');
+
+Route::get('/blog/{name}', function($name) {
+    return "This is $name's blog page.";
+});
+
+Route::get('/user/{name?}', function($name = '|John') {
+    return "User = $name";
+});
