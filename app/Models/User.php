89a7aa1d708c5d7a0934272709faa0b3b8ad311f
sjain882 <43217178+sjain882@users.noreply.github.com>
2023-10-20 17:45:53 +0100

    Adjust User properties

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,10 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
-        'email',
+        'username',
         'password',
+        'picture',
+        'role_id',
     ];
 
     /**
@@ -39,7 +40,6 @@ class User extends Authenticatable
      * @var array<string, string>
      */
     protected $casts = [
-        'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
 }
