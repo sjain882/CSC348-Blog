3f38f044c316b4c9e0c4dfaff44004aa2b71d520
sjain882 <43217178+sjain882@users.noreply.github.com>
2023-11-01 19:46:50 +0000

    Implement seeding of roles

--- database/migrations/2023_10_20_162618_create_roles_table.php
+++ database/migrations/2023_10_20_162618_create_roles_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('roles', function (Blueprint $table) {
-            $table->id('id');
+            $table->increments('id');
             $table->string('name');
             $table->timestamps();
         });
--- database/seeders/RolesTableSeeder.php
+++ database/seeders/RolesTableSeeder.php
@@ -12,6 +12,26 @@ class RolesTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        
+        // Create admin role
+        $a = new Role;
+        $a->name = "Admin";
+        $a->save();
+
+        // Create moderator role
+        $a = new Role;
+        $a->name = "Moderator";
+        $a->save();
+
+        // Create user role
+        $a = new Role;
+        $a->name = "User";
+        $a->save();
+
+        // Create muted role
+        $a = new Role;
+        $a->name = "Muted";
+        $a->save();
+        
     }
 }
