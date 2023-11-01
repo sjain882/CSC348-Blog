94505cecff5da8af1616aa804d3cc87ff999d1c1
sjain882 <43217178+sjain882@users.noreply.github.com>
2023-11-01 17:42:21 +0000

    Setup roles table content

--- database/migrations/2023_10_20_162618_create_roles_table.php
+++ database/migrations/2023_10_20_162618_create_roles_table.php
@@ -12,7 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('roles', function (Blueprint $table) {
-            $table->id();
+            $table->id('id');
+            $table->string('name');
             $table->timestamps();
         });
     }
