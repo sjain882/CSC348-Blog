2ad59f1d33ff2c49dd3c3ff29014c26f847819bf
sjain882 <43217178+sjain882@users.noreply.github.com>
2023-11-02 21:26:36 +0000

    Add default role value of 'user' for users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('picture')->nullable();
-            $table->unsignedInteger('role_id');
+            $table->unsignedInteger('role_id')->default(3);
             $table->rememberToken();
             $table->timestamps();
         });
