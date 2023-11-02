355a10424b3f6a6a3b37336719834245d8b4a5bc
sjain882 <43217178+sjain882@users.noreply.github.com>
2023-11-02 21:48:08 +0000

    Remove redundant columns in lieu of proper relations

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,7 +22,6 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'role_id' => fake()->numberBetween(2,((Role::get()->count()))),
             'password' => bcrypt('changeme'), // password
         ];
     }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('picture')->nullable();
-            $table->unsignedInteger('role_id')->default(3);
             $table->rememberToken();
             $table->timestamps();
         });
