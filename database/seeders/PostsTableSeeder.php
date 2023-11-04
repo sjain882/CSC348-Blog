d1d2c14940b67d68e6217ff7ff6ce10a967fba4b
sjain882 <43217178+sjain882@users.noreply.github.com>
2023-11-04 20:11:47 +0000

    Implement basic Posts table seeder

--- database/seeders/PostsTableSeeder.php
+++ database/seeders/PostsTableSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Model\Post;
+use App\Model\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,7 +14,30 @@ class PostsTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // Create the first post
+        $firstPost = new Post;
+        $firstPost->title = "Hello world!";
+        $firstPost->body = "This is the first ever blog post!";
+        $firstPost->image_path = null;
+        $firstPost->user_id = 0;        // Default admin user
+        $firstPost->save();
+
+        // Create a post made by a random user
+        $secondPost = new Post;
+        $secondPost->title = "Greetings all!";
+        $secondPost->body = "This is the second ever blog post!";
+        $secondPost->image_path = null;
+        $secondPost->user_id = fake()->numberBetween(1, (User::get()->count())); // Random user
+        $secondPost->save();
+
+        // Create a post made by a random user with random contents
+        $secondPost = new Post;
+        $secondPost->title = faker()->lorem()->sentence(1);
+        $secondPost->body = faker()->lorem()->paragraph(2);
+        $secondPost->image_path = null;
+        $secondPost->user_id = fake()->numberBetween(1, (User::get()->count())); // Random user
+        $secondPost->save();
+
     }
 
 }
