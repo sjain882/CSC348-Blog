5cea5c911af489d30efe5f0de6f2be463aaebad5
sjain882 <43217178+sjain882@users.noreply.github.com>
2023-11-05 01:02:54 +0000

    Add random fake number seeding for 1 user

--- database/seeders/PhoneTableSeeder.php
+++ database/seeders/PhoneTableSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Phone;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -19,6 +20,12 @@ class PhoneTableSeeder extends Seeder
         $firstNumber->phone_num = "+44 1632 960413";
         $firstNumber->user_id = 1;        // Default admin user
         $firstNumber->save();
+
+        // Add a random fake phone number to a random user
+        $secondNumber = new Phone;
+        $secondNumber->phone_num = fake()->unique()->e164PhoneNumber();
+        $secondNumber->user_id = fake()->numberBetween(1, (User::get()->count())); // Random user
+        $secondNumber->save();
 
     }
 }
