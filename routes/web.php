c1af02fb0dc7e0e53b9253c4a32e0310f6a47152
sjain882 <43217178+sjain882@users.noreply.github.com>
2023-12-03 21:01:25 +0000

    Add routes

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,68 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+
+// Redirect to login if not logged in, or homepage if logged in
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Main page with everyone's posts
+Route::get('/home', function () {
+    return "This is the blog's home page.";
+});
+
+/*
+// Login existing user
+Route::get('/login', function () {
+    return view('login');
+});
+
+// Register new user
+Route::get('/register', function () {
+    return view('register');
+});
+
+// Forgot password
+Route::get('/passwordreset', function () {
+    return view('passwordreset');
+});
+*/
+
+// Create new post
+Route::get('/newpost', function () {
+    return view('newpost');
+});
+
+// View a user's profile, posts & comments
+Route::get('/profile/{username}', function($name) {
+    return "This is $username's blog page.";
+});
+
+// Direct link to a post
+Route::get('/viewpost/{postid}', function () {
+    return "Post ID: $postid";
+});
+
+// Direct link to a comment within a post
+Route::get('/viewpost/{postid}#{commentid}', function () {
+    return "Comment ID: $commentid";
+});
+
+// Redirect users looking for the 'blog' page to the correct home page
+Route::redirect('/blog', '/home');
+
+// Nullable example
+Route::get('/user/{name?}', function($name = '|John') {
+    return "User = $name";
+});
+
+
+
+
+
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
