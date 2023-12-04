ad925d86377fb4e07b6149ea7bce5ca4efc1b5bc
sjain882 <43217178+sjain882@users.noreply.github.com>
2023-12-04 00:39:56 +0000

    Create main layout for all blog pages

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+
+<html lang="en">
+
+    <head>
+        <title>ModernBlog - @yield('title')</title>
+    </head>
+
+
+    <body>
+
+        <h1>ModernBlog - @yield('title')</h1>
+
+        <div>
+            @yield('content')
+        </div>
+
+    </body>
+    
+</html>
