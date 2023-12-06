cfaf3f55d896d8a928613a439a8519e51cd917f7
sjain882 <43217178+sjain882@users.noreply.github.com>
2023-12-06 17:32:04 +0000

    Add hyperlink to user linked to post

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -10,8 +10,7 @@
             <li>Title: {{ $post -> title }}</a></li>
             <li>Body: {{ $post -> body }}</a></li>
             <li>Image Path: {{ $post -> image_path ?? 'No image uploaded' }}</a></li>
-            <li>Posted by user id : {{ $post -> user_id }}</a></li>
-            <li>Posted by user name : {{ $post -> user -> name }}</a></li>
+            <li>Posted by user:  <a href="/user/{{ $post -> user -> id }}">{{ $post -> user -> name }}</a></li>
         @endforeach
 
     </ul>
