a2e11cc4c60891b1134d081512dfd20132df320c
sjain882 <43217178+sjain882@users.noreply.github.com>
2023-12-08 21:11:26 +0000

    Mods can delete posts too now

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -129,7 +129,7 @@ class PostController extends Controller
         $userid = Auth::id();
         $post = Post::findOrFail($id);
 
-        if ($userid == $post->user_id || $user->isAdmin())
+        if ($userid == $post->user_id || $user->isAdmin() || $user->isModerator())
         {
             $post->delete();
             return redirect()->route('post.index')->with('message', 'Post was deleted.');
