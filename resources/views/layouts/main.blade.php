05cc91571919433cb41724e7cd13cbeed7b91cef
sjain882 <43217178+sjain882@users.noreply.github.com>
2023-12-09 23:41:10 +0000

    Implement navbar

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,16 +1,34 @@
 <!doctype html>
 
 <html lang="en">
+  
+
+
 
     <head>
         <title>ModernBlog - @yield('title')</title>
         @vite(['resources/css/app.css','resources/js/app.js'])
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/flowbite.min.css" rel="stylesheet" />
+
+
+
+          
+
+
+
+
+
     </head>
 
 
     <body>
+        
 
-        <h1>ModernBlog - @yield('title')</h1>
+
+
+
+
+        {{-- <h1>ModernBlog - @yield('title')</h1> --}}
 
         @if (session('message'))
 
@@ -37,7 +55,7 @@
             @yield('content')
         </div>
 
-
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/flowbite.min.js"></script>
     </body>
     
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,14 @@ import forms from '@tailwindcss/forms';
 
 export default {
 
-
+    darkMode: "class",
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         "./resources/**/*.blade.php",
         "./resources/**/*.js",
         "./resources/**/*.vue",
+        "./node_modules/flowbite/**/*.js"
     ],
 
     theme: {
