b375849cf5604759cc528d271e4ca3bfeac22859
sjain882 <43217178+sjain882@users.noreply.github.com>
2023-12-09 23:58:48 +0000

    Setup create form css

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -38,21 +38,27 @@
     <form method="POST" action="{{ route('post.store') }}" enctype="multipart/form-data">
 
         @csrf
-        <p>Title: <input type="text" name="title"
 
-            value="{{ old('title') }}"></p>
+        <div class="mb-5">
+            <label for="large-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title</label>
+            <input type="text" id="title" name="title" value="{{ old('title') }}" class="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+        </div>
 
-        <p>Body: <input type="text" name="body"
+        <div class="mb-5">
+            <label for="large-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Body</label>
+            <input type="text" id="body" name="body" value="{{ old('body') }}" class="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+        </div>
 
-            value="{{ old('body') }}"></p>
+        
+        <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="file_input">Upload file</label>
+        <input aria-describedby="file_input_help" id="image" name="image" type="file" class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400">
+        <p class="mt-1 text-sm text-gray-500 dark:text-gray-300" id="file_input_help">JPEG, JPG or PNG (MAX. 2000x2000px).</p>
 
-        <p>Image: <input type="file" name="image"
+        <br>
 
-            value="{{ old('image') }}"></p>
-    
-        <input type="submit" value="Submit">
+        <input type="submit" value="Submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
 
-        <a href="{{ route('post.index') }}">Cancel</a>
+        <a href="{{ route('post.index') }}" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Cancel</a>
 
     </form>
 
