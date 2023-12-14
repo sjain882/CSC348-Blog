e7f86e37056225aac9ab595127cb25e24c725fad
sjain882 <43217178+sjain882@users.noreply.github.com>
2023-12-14 12:09:36 +0000

    Display comments

--- resources/views/post/single.blade.php
+++ resources/views/post/single.blade.php
@@ -62,6 +62,41 @@
 
 
 
+    <header>
+        <h1>Comments:</h1>
+    </header>
+
+
+
+    @foreach ($post->comments as $comment)
+
+        <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+
+            @if(false)
+                <a href="{{ route('post.show', ['id' => $comment->id]) }}">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $comment -> id }}</h5>
+                </a>
+            @endif
+
+            <div class="p-5">
+
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ $comment -> body }}</p>
+
+                <a href="/user/{{ $comment -> user -> id }}" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    Posted by {{ $comment -> user -> name }}
+                    <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                    </svg>
+                </a>
+
+            </div>
+        </div>
+
+        <br>
+        <br>
+        
+    @endforeach
+
 
 @endsection
 
